0c2a5a4adeead2c1609474294a12f68e4dd19eae
allysarh <allysa.rahagustiani@gmail.com>
2021-07-19 23:26:37 +0700

    BE-ALLYSA-11: PHR-6 Fix bug

--- controller/userController.js
+++ controller/userController.js
@@ -4,41 +4,54 @@ const Crypto = require('crypto')
 module.exports = {
     login: async (req, res, next) => {
         try {
-            let { email, password } = req.body
+             let { email, password } = req.body
             let hashPassword = Crypto.createHmac("sha256", "PHR$$$").update(password).digest("hex")
+            // email check
+            let emailCheck = `SELECT * from user where email = ${db.escape(email)};`
+            emailCheck = await dbQuery(emailCheck)
 
-            let login = `SELECT iduser, fullname, gender, age, username, email, role, status, profile_image, otp 
-            from user as u
-            LEFT join role as r
-            on r.idrole = u.idrole
-            left join status as s
-            on s.idstatus = u.idstatus
-            where email = ${db.escape(email)} and password = ${db.escape(hashPassword)};`
-            login = await dbQuery(login)
+            if (emailCheck.length > 0) {
 
-            console.log(login)
-            login[0].cart = []
-            login[0].address = []
-            
-            let { iduser, fullname, gender, age, username, role, status, profile_image, cart, address, otp } = login[0]
-            let token = createToken({ iduser, fullname, gender, age, username, role, status, profile_image, otp })
+                let login = `SELECT iduser, fullname, gender, age, username, email, role, status, profile_image, otp 
+                from user as u
+                LEFT join role as r
+                on r.idrole = u.idrole
+                left join status as s
+                on s.idstatus = u.idstatus
+                where email = ${db.escape(email)} and password = ${db.escape(hashPassword)};`
+                login = await dbQuery(login)
 
-            let getCart = `SELECT * from cart where iduser = ${iduser};`
-            getCart = await dbQuery(getCart)
+                if (login.length > 0) {
+                    login[0].cart = []
+                    login[0].address = []
+                    let { iduser, fullname, gender, age, username, role, status, profile_image, cart, address, otp } = login[0]
+                    if (status == "verified") {
+                        let token = createToken({ iduser, fullname, gender, age, username, role, status, profile_image, otp })
 
-            getCart.forEach((item, index) => {
-                cart.push(item)
-            })
+                        let getCart = `SELECT * from cart where iduser = ${iduser};`
+                        getCart = await dbQuery(getCart)
 
-            let getAddress = `SELECT * from address where iduser = ${iduser};`
-            getAddress = await dbQuery(getAddress)
+                        getCart.forEach((item, index) => {
+                            cart.push(item)
+                        })
 
-            getAddress.forEach((val, i) => {
-                address.push(val)
-            })
+                        let getAddress = `SELECT * from address where iduser = ${iduser};`
+                        getAddress = await dbQuery(getAddress)
 
-            res.status(200).send({ iduser, fullname, gender, age, username, role, status, profile_image, cart, address, token })
-            // res.status(200).send(login)
+                        getAddress.forEach((val, i) => {
+                            address.push(val)
+                        })
+                        let response = 200
+                        res.status(200).send({ iduser, fullname, gender, age, username, role, status, profile_image, cart, address, token, response })
+                    } else {
+                        res.status(200).send({ response: 400, status: "unverified", messages: "Account not verified! Please verify",  })
+                    }
+                } else {
+                    res.status(200).send({ response: 400, messages: "Wrong password!" })
+                }
+            } else {
+                res.status(200).send({ response: 400 , messages: "Account not found! Please register" })
+            }
         } catch (error) {
             next(error)
         }
