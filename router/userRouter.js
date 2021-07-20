68c14ba3c5e6108783b812a1d07f91e83a0c092d
allysarh <allysa.rahagustiani@gmail.com>
2021-07-20 20:35:56 +0700

    Fix bugs

--- router/userRouter.js
+++ router/userRouter.js
@@ -3,7 +3,7 @@ const { readToken } = require('../config')
 const router = express.Router()
 const { userController } = require('../controller')
 
-router.get('/verif', readToken, userController.accVerif)
+router.post('/verif', readToken, userController.accVerif)
 router.post('/re-verif', userController.reVerif)
 
 module.exports = router
