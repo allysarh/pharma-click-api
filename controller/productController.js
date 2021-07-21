4407748e2ae8ca2187cff5911a44b23e0cda7f46
allysarh <allysa.rahagustiani@gmail.com>
2021-07-21 20:34:50 +0700

    BE-ALLYSA-11: Memperbaiki API untuk get dan add product

--- controller/productController.js
+++ controller/productController.js
@@ -4,21 +4,34 @@ const { dbQuery, db } = require('../config/database')
 
 
 module.exports = {
-    getProduct: async (req, res) => {
+    getProductPack: async (req, res, next) => {
         try {
-            let getProduct = `SELECT * from product`
+            
+            let getProduct = `SELECT idproduct, product_name, brand, category, description, effect, p.usage, dosage, indication from product as p
+            LEFT join category as c on c.idcategory = p.idcategory
+            LEFT join status as s on s.idstatus = p.idstatus;;`
             if (Object.keys(req.query).length > 0) {
                 let searchQuery = []
                 for (props in req.query) {
                     searchQuery.push(`${props} = ${db.escape(req.query[props])}`)
                 }
-                getProduct = `SELECT * from product WHERE ${searchQuery.join(" AND ")};`
-                console.log(getProduct)
+                getProduct = `SELECT idproduct, product_name, brand, category, description, effect, p.usage, dosage, indication from product as p
+                LEFT join category as c on c.idcategory = p.idcategory
+                LEFT join status as s on s.idstatus = p.idstatus WHERE ${searchQuery.join(" AND ")};`
             }
 
             getProduct = await dbQuery(getProduct)
-
-            let getStock = `SELECT * from stock`
+            let getStock
+            if(req.params.idtype == 'all') {
+                getStock = `SELECT idstock, idproduct, status, netto, qty, total_netto, unit_price, pack_price, unit, type from stock as st
+                LEFT join type as t on t.idtype = st.idtype
+                LEFT join status as s on s.idstatus = st.idstatus;`
+            } else {
+                getStock = `SELECT idstock, idproduct, status, netto, qty, total_netto, unit_price, pack_price, unit, type from stock as st
+                LEFT join type as t on t.idtype = st.idtype
+                LEFT join status as s on s.idstatus = st.idstatus
+                WHERE st.idtype = ${req.params.idtype};`
+            }
             getStock = await dbQuery(getStock)
 
             let getImg = `SELECT * from product_image`
@@ -48,14 +61,14 @@ module.exports = {
     },
     addProduct: async (req, res, next) => {
         try {
-            console.log("hayoloh")
-            let { product_name, brand, idstatus, idcategori, stock, description, effect, dosage, usage, indication } = JSON.parse(req.body.data)
-            console.log(product_name)
+            // console.log("hayoloh")
+            // let { product_name, brand, idstatus, idcategori, stock, description, effect, dosage, usage, indication } = JSON.parse(req.body.data)
+            // console.log(product_name)
             // const { images } = req.files
-            console.log(req.files)
+            // console.log(req.files)
             // let addProduct = `INSERT into product (product_name, brand, idstatus, category, description, effect, usage, dosage, indication) values (${db.escape(product_name)}, ${db.escape(brand)}, '3', ${db.escape(category)}, ${db.escape(description)}, ${db.escape(effect)}, ${db.escape(usage)}, ${db.escape(dosage)}, ${db.escape(indication)})`
             // const upload = uploader('/products', 'IMG').fields([{ name: 'products' }])
-            res.status(200).send("OK")
+            // res.status(200).send("OK")
 
         } catch (error) {
 
--- router/productRouter.js
+++ router/productRouter.js
@@ -2,7 +2,7 @@ const express = require('express')
 const { productConctroller, productController } = require('../controller')
 const router = express.Router()
 
-router.get('/get', productController.getProduct)
+router.get('/get/:idtype', productController.getProductPack)
 router.post('/add', productController.addProduct)
 
 module.exports = router
