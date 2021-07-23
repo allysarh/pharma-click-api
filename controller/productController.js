785013716a06d2d50d9f7b110318c3a81f6dfba6
allysarh <allysa.rahagustiani@gmail.com>
2021-07-23 13:34:48 +0700

    fix bug

--- controller/productController.js
+++ controller/productController.js
@@ -65,7 +65,7 @@ module.exports = {
                 try {
                     const { products } = req.files
                     let { product_name, brand, idcategory, description, effect, usage, dosage, indication, netto, pack_price, unit, stock } = JSON.parse(req.body.data)
-                    let addProduct = `INSERT into product values (null, ${db.escape(product_name)}, ${db.escape(brand)}, 1,${db.escape(idcategory)}, ${db.escape(description)}, ${db.escape(effect)}, ${db.escape(usage)}, ${db.escape(dosage)}, ${db.escape(indication)}, ${db.escape(netto)}, ${db.escape(pack_price)}, ${db.escape(unit)}, now(), now());`
+                    let addProduct = `INSERT into product values (null, ${db.escape(product_name)}, ${db.escape(brand)}, ${db.escape(idcategory)}, ${db.escape(description)}, ${db.escape(effect)}, ${db.escape(usage)}, ${db.escape(dosage)}, ${db.escape(indication)}, ${db.escape(netto)}, ${db.escape(pack_price)}, ${db.escape(unit)}, now(), now());`
                     addProduct = await dbQuery(addProduct)
 
                     let addImg = `INSERT into product_image values (null, ${addProduct.insertId}, ${db.escape(`products/${products[0].filename}`)})`
