0f984019529ffb77a77072370282a05c53a858a1
mrakhal <mochamadrakha@gmail.com>
2021-08-03 17:13:41 +0700

    BE-RAKHA-PHR-28 : As an admin, I want to record raw materials/medicines stock using bottles as the main unit of measurement

--- controller/productController.js
+++ controller/productController.js
@@ -136,6 +136,60 @@ module.exports = {
       next(error);
     }
   },
+  addCustomProduct: async (req, res, next) => {
+    try {
+      const upload = uploader("/products", "IMG").fields([
+        { name: "products" },
+      ]);
+      upload(req, res, async (error) => {
+        try {
+          const { products } = req.files;
+          let {
+            product_name,
+            brand,
+            idcategory,
+            description,
+            effect,
+            usage,
+            dosage,
+            indication,
+            netto,
+            pack_price,
+            unit,
+            stock,
+          } = JSON.parse(req.body.data);
+          let addProduct = `INSERT into product values (null, ${db.escape(
+            product_name
+          )}, ${db.escape(brand)}, ${db.escape(idcategory)}, ${db.escape(
+            description
+          )}, ${db.escape(effect)}, ${db.escape(usage)}, ${db.escape(
+            dosage
+          )}, ${db.escape(indication)}, ${db.escape(netto)}, ${db.escape(
+            pack_price
+          )}, ${db.escape(unit)}, now(), now());`;
+          addProduct = await dbQuery(addProduct);
+
+          let addImg = `INSERT into product_image values (null, ${addProduct.insertId
+            }, ${db.escape(`products/${products[0].filename}`)})`;
+          await dbQuery(addImg);
+
+          let addStock = `INSERT into stock values (null, ${addProduct.insertId
+            }, 2, ${db.escape(stock[0].qty)}, ${db.escape(
+              stock[0].total_netto
+            )}, null, 1);`;
+          await dbQuery(addStock);
+
+          res
+            .status(200)
+            .send({ status: 200, messages: "Add product berhasil!" });
+        } catch (error) {
+          next(error);
+        }
+      })
+    } catch (error) {
+      next(error);
+    }
+  },
   deleteProduct: async (req, res, next) => {
     try {
       let deleteProduct = `UPDATE stock set idstatus = 3 where id = ${db.escape(
@@ -232,6 +286,91 @@ module.exports = {
           await dbQuery(editProduct);
           let total_netto = stock[0].qty * netto;
           let editStock = `UPDATE stock set idtype = 1, qty = ${db.escape(
+            stock[0].qty
+          )}, total_netto = ${db.escape(
+            total_netto
+          )}, idstatus = 1 where id = ${db.escape(stock[0].id)};`;
+
+          await dbQuery(editStock);
+
+          let getImg = `SELECT * from product_image where idproduct = ${stock[0].idproduct};`;
+          getImg = await dbQuery(getImg);
+          if (req.files.products) {
+            console.log(getImg[0].image_url);
+            console.log(getImg[0].image_url.includes("http"));
+            if (getImg[0].image_url.includes("http") == false) {
+              fs.unlinkSync(`./public/${getImg[0].image_url}`);
+            }
+            const { products } = req.files;
+            let updateImage = `Update product_image set image_url = ${db.escape(
+              `products/${products[0].filename}`
+            )} where idproduct = ${idproduct};`;
+            await dbQuery(updateImage);
+          }
+
+          res
+            .status(200)
+            .send({ status: 200, messages: "Edit product berhasil!" });
+        } catch (error) {
+          // hapus gambar yang sudah di upload
+          if (req.files.products[0].originalname.match(/(jpg|jpeg|png)/)) {
+            fs.unlinkSync(
+              `./public/products/${req.files.products[0].filename}`
+            );
+            // error db query
+            console.log("error catch", error);
+          } else {
+            console.log("invalid file format", error);
+          }
+          res.status(400).send("ERROR");
+        }
+      });
+    } catch (error) {
+      next(error);
+    }
+  },
+  editProductCustom: async (req, res, next) => {
+    try {
+      const upload = uploader("/products", "IMG").fields([
+        { name: "products" },
+      ]);
+      upload(req, res, async (error) => {
+        try {
+          console.log("edit");
+          let {
+            idproduct,
+            product_name,
+            brand,
+            idcategory,
+            description,
+            effect,
+            usage,
+            dosage,
+            indication,
+            netto,
+            pack_price,
+            unit,
+            stock,
+          } = JSON.parse(req.body.data);
+          let editProduct = `UPDATE product p set product_name = ${db.escape(
+            product_name
+          )}, brand = ${db.escape(brand)}, idcategory = ${db.escape(
+            idcategory
+          )}, 
+                    description = ${db.escape(
+            description
+          )}, effect = ${db.escape(effect)}, p.usage = ${db.escape(
+            usage
+          )}, dosage = ${db.escape(dosage)}, indication = ${db.escape(
+            indication
+          )}, 
+                    netto = ${db.escape(netto)}, pack_price = ${db.escape(
+            pack_price
+          )}, unit = ${db.escape(unit)} where id = ${idproduct};`;
+
+          await dbQuery(editProduct);
+          let total_netto = stock[0].qty * netto;
+          let editStock = `UPDATE stock set idtype = 2, qty = ${db.escape(
             stock[0].qty
           )}, total_netto = ${db.escape(
             total_netto
